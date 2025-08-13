6ad69cfb1ffc36370a60e712ed6ea24a11df6d60
Chloe <chloeheewon0615@gmail.com>
2025-08-13 15:50:09 +0900

    [fea] #36 - 앱 실행을 위한 run.py 생성

--- backend/run.py
+++ backend/run.py
@@ -1,1 +1,13 @@
-# git test
+# 실행방법 : bash에 "python -m backend.run" 입력 
+
+"""Module providing a function printing python version."""
+import os
+from backend.app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", "5000"))
+    debug = os.getenv("FLASK_DEBUG", "1") == "1"
+    app.run(host=host, port=port, debug=debug)
